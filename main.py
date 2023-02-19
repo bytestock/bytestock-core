c10da6754418cf2542d6ea4ff7b9afb81968c1d1
5late <christianrfernandes5@gmail.com>
2023-02-18 23:18:04 -0500

    added env

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import yfinance as yf
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 goog = yf.Ticker("GOOG")
 goog.fast_info
 
 
-data = goog.history(period="1mo")
+data = goog.history(period="1y")
 #shares = msft.get_shares_full(start = "2022-01-01", end = None)
 
 print(data)

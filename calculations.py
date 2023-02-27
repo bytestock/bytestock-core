9082d24495178af47b98fb47fd32224b7b9dd697
Ryan A <alumkalryan@gmail.com>
2023-02-26 22:52:28 -0500

    added to calculations

--- calculations.py
+++ calculations.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def daily_ratio_calculation(close_data):
     daily_ratio_values = []
     adj_close_count = 0
@@ -13,7 +12,7 @@ def daily_ratio_calculation(close_data):
                 daily_ratio = (current_adj_close/prev_adj_close)
                 daily_ratio_values.append(daily_ratio)
             except:
-                print("Error with daily ratio {i}")
+                pass #weekdays/days that the market is closed
             prev_adj_close = current_adj_close
     return daily_ratio_values
 
@@ -29,7 +28,7 @@ def weekly_ratio_calculation(close_data):
                 weekly_ratio = numerator_daily/denominator_daily
                 weekly_ratio_values.append(weekly_ratio)
             except:
-                print(f"Error with weekly ratio {i}")
+                pass #weekdays/days that the market is closed
     return weekly_ratio_values
 
 def daily_ratio_average_calculations(daily_ratio):
@@ -40,11 +39,12 @@ def daily_ratio_average_calculations(daily_ratio):
         daily_ratio_average_sum = 0
         if daily_ratio_count > 4:
             try:
-                daily_ratio_average_sum +=daily_ratio[i:i-4]
+                for n in range((i-4),i+1):
+                    daily_ratio_average_sum +=daily_ratio[n]
                 daily_ratio_average = daily_ratio_average_sum/5
                 daily_ratio_average_values.append(daily_ratio_average)
             except:
-                print(f"Error with daily ratio average {i}")
+                pass
     return daily_ratio_average_values
 
 def daily_ratio_standard_deviation_calculation(daily_ratio):
@@ -52,19 +52,27 @@ def daily_ratio_standard_deviation_calculation(daily_ratio):
     daily_ratio_count = 0
     for i in range(len(daily_ratio)):
         daily_ratio_count +=1
-        if daily_ratio_count > 4:
+        daily_ratio__sum = 0
+        if daily_ratio_count >4:
             try:
-                #daily_ratio_standard_deviation = daily_ratio[i-4]
-                daily_ratio_standard_deviation = np.std(daily_ratio[i:1-4])
+                #print(f"daily_ratio_count: {daily_ratio_count}")
+                #print(f"i: {i}")
+                #for n in range((i-4),i+1):
+                    #daily_ratio__sum +=daily_ratio[n]
+                daily_ratio_standard_deviation = np.std(5, daily_ratio[(i-4),i+1])# something wrong with standard deviation, how does it work?
+                print(daily_ratio_standard_deviation)
                 daily_ratio_standard_deviation_values.append(daily_ratio_standard_deviation)
             except:
-                print("Error with daily ratio standard deviation{i}")
+                pass
+                #print("Error with daily ratio standard deviation{i}")
     return daily_ratio_standard_deviation_values
 
 def mathematics(close_data):
     daily_ratio = daily_ratio_calculation(close_data) #correct
     weekly_ratio = weekly_ratio_calculation(close_data) #correct
-    daily_ratio_average = daily_ratio_average_calculations(daily_ratio) #CHECK, SUM WRONG
+    daily_ratio_average = daily_ratio_average_calculations(daily_ratio) #works
     daily_ratio_standard_deviation = daily_ratio_standard_deviation_calculation(daily_ratio) #CHECK, SUM WRONG
-    print(f"daily ratio: {daily_ratio}")
-    print(f"daily ratio average: {daily_ratio_average}")
+    print(f"Close data: {close_data}")
+    print(f"Daily ratio : {daily_ratio}")
+    print(f"Daily ratio standard deviation: {daily_ratio_standard_deviation}")
+    #print(f"daily ratio average: {daily_ratio_average}")

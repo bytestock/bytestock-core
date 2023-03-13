e072bc533ed0286c626da833625367eb605ac616
5late <christianrfernandes5@gmail.com>
2023-03-12 23:23:19 -0400

    added support for adjusted close

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import time
 import json
 import misc
+import datetime
 
 load_dotenv()
 
@@ -15,22 +16,26 @@ load_dotenv()
 ticks = int(time.time())
 
 def getOCHLData(ticker, days):
-    start = days * 24 * 60 * 60
+    now = datetime.datetime.now()
+    d = datetime.timedelta(days = days)
+    start = now - d
+    start = start.strftime('%Y-%m-%d')
 
     rate_limit_free = misc.telemetry(ticker, days)
     if rate_limit_free:
         #data = finnhub_client.stock_candles(ticker, 'D', ticks - start, ticks)
         stock = yf.Ticker(ticker)
-        data = stock.history(period=f'{days}d')
+        data = yf.download(ticker, start=start, end=now.strftime('%Y-%m-%d'))
     
     #open_days, closed_days = misc.wasMarketClosedFrom(ticks - start, ticks, days-1)
     open_days = data.index.tolist()
     daily_close = list(data['Close'])
+    daily_adj_close = list(data['Adj Close'])
     daily_open = list(data['Open'])
     daily_high = list(data['High'])
     daily_low = list(data['Low'])
 
-    return open_days, daily_open, daily_close, daily_high, daily_low
+    return open_days, daily_open, daily_close, daily_adj_close, daily_high, daily_low
 
 def getRealTimeOCHL(ticker, days):
     #data = finnhub_client.quote(ticker)

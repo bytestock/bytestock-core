d6ca3fc41f847e97bc185e396638748b166d5829
5late <christianrfernandes5@gmail.com>
2023-03-12 23:10:00 -0400

    migrate from finnhub to yfinance library

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
-import finnhub
+#import finnhub
+import yfinance as yf
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -8,8 +9,8 @@ import misc
 
 load_dotenv()
 
-API_KEY=st.secrets['API_KEY']
-finnhub_client = finnhub.Client(api_key=API_KEY)
+#API_KEY=st.secrets['API_KEY']
+#finnhub_client = finnhub.Client(api_key=API_KEY)
 
 ticks = int(time.time())
 
@@ -18,24 +19,32 @@ def getOCHLData(ticker, days):
 
     rate_limit_free = misc.telemetry(ticker, days)
     if rate_limit_free:
-        data = finnhub_client.stock_candles(ticker, 'D', ticks - start, ticks)
-
-    open_days, closed_days = misc.wasMarketClosedFrom(ticks - start, ticks, days-1)
-    daily_close = data.get('c')
-    daily_open = data.get('o')
-    daily_high = data.get('h')
-    daily_low = data.get('l')
+        #data = finnhub_client.stock_candles(ticker, 'D', ticks - start, ticks)
+        stock = yf.Ticker(ticker)
+        data = stock.history(period=f'{days}d')
+    
+    #open_days, closed_days = misc.wasMarketClosedFrom(ticks - start, ticks, days-1)
+    open_days = data.index.tolist()
+    daily_close = list(data['Close'])
+    daily_open = list(data['Open'])
+    daily_high = list(data['High'])
+    daily_low = list(data['Low'])
 
     return open_days, daily_open, daily_close, daily_high, daily_low
 
-def getRealTimeOCHL(ticker):
-    data = finnhub_client.quote(ticker)
+def getRealTimeOCHL(ticker, days):
+    #data = finnhub_client.quote(ticker)
+    rate_limit_free = misc.telemetry(ticker, days)
+    if rate_limit_free:
+        stock = yf.Ticker(ticker)
+        data = stock.fast_info
+    
+    rt_previous_close = list(stock.history(period=f'2d')['Close'])[-2]
+    rt_open = data.open
+    rt_current = data.last_price
+    rt_high = data.day_high
+    rt_low = data.day_low
+    rt_change = rt_current - data.previous_close
+    rt_change_percent = round((rt_current - rt_previous_close)/((rt_current + rt_previous_close) / 2) * 100, 2)
 
-    rt_open = data.get('o')
-    rt_current = data.get('c')
-    rt_high = data.get('h')
-    rt_low = data.get('l')
-    rt_change = data.get('d')
-    rt_change_percent = data.get('dp')
-
-    return rt_open, rt_current, rt_high, rt_low, rt_change, rt_change_percent
+    return rt_previous_close, rt_open, rt_current, rt_high, rt_low, rt_change, rt_change_percent

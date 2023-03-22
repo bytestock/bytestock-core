b766d8f42657e31b528e1863391e4faaefcbfdd7
Ryan A <82676662+ryanalumkal@users.noreply.github.com>
2023-03-22 15:21:11 +0000

    cleaned up some more

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+"""Libraries"""
 import calculations
 import data
 
 
-def main():
+def main()->None:
+    """Main Function"""
     while True:
         try:
             user_input = input("Enter a stock: ")
             days = int(input("Enter preferred days: "))
             if days >0:
                 break
-        except:
+        except ValueError:
             print("Enter a valid amount of days")
     open_days, daily_open, daily_close, daily_adj_close, daily_high, daily_low = data.getOCHLData(user_input, days) #Not close data, rather, open; we need Adj Close
-    #print(close_data)
     calculations.mathematics(daily_adj_close)
 
+"""Main Program"""
 if __name__ == "__main__":
     main()

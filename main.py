69144833a349c1c41c606dcd56616a9873e29692
Nolawi Teklehaimanot <110188368+nolawiyonas1@users.noreply.github.com>
2023-03-30 19:12:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 """Libraries"""
-import calculations
-import data
+import calculations # importing the calculations module to use its functions
+import data # importing the data module to use its functions
 
 
-def main()->None:
+def main()->None: # creating the main() functions
     """Main Function"""
-    while True:
-        try:
-            user_input = input("Enter a stock: ")
-            days = int(input("Enter preferred days: "))
-            if days >0:
+    while True: # infinite loop
+        try: # ensuring that the user enters a valid number
+            user_input = input("Enter a stock: ") # asking the user to enter a stock
+            days = int(input("Enter preferred days: ")) # asking the user for the number of days
+            if days >0: # break if the number of days is greater than 0
                 break
-        except ValueError:
+        except ValueError: # asking the user to enter a valid number of days
             print("Enter a valid amount of days")
     open_days, daily_open, daily_close, daily_adj_close, daily_high, daily_low = data.getOCHLData(user_input, days) #Not close data, rather, open; we need Adj Close
     with open("close-data.txt", "w") as f:
@@ -23,4 +23,4 @@ def main()->None:
 
 """Main Program"""
 if __name__ == "__main__":
-    main()
+    main() # calling the main function

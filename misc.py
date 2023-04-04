851eadaa83d5248c290a0c24efd9e5165a2e0a11
Ryan A <82676662+ryanalumkal@users.noreply.github.com>
2023-04-04 09:57:18 -0400

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,75 +1,83 @@
 from datetime import datetime
 import time
 
-def getDateTimefromTicks(ticks):
-    date = datetime.fromtimestamp(ticks).strftime('%Y-%m-%d %H:%M:%S')
 
-    return date
-
-def getFancyDateTimefromTicks(ticks):
-    date = datetime.fromtimestamp(ticks).strftime('%b %d %Y')
-
-    return date
-
-def getDayFromDate(date):
-    day = datetime.strptime(date, '%Y-%m-%d').strftime('%A')
-
-    return day
-
-def getMarketCloseDates():
-    with open('market-closed-dates.txt') as file:
-        lines = file.readlines()
-
-    return lines
-
-def isMarketClosed(ticks):
-    date = getDateTimefromTicks(ticks).split(' ')[0]
+class Misc:
+    def __init__(self,ticks, date, start, end) -> None:
+        self.ticks = ticks
+        self.date = date
+        self.start = start
+        self.end = end
     
-    market_closed_dates = getMarketCloseDates()
-    day = getDayFromDate(date)
+    def getDateTimefromTicks(self):
+        date = datetime.fromtimestamp(self.ticks).strftime('%Y-%m-%d %H:%M:%S')
 
-    if date in market_closed_dates:
-        return True
-    elif day == 'Saturday' or day == 'Sunday':
-        return True
-    else:
-        return False
+        return date
+    
+    def getFancyDateTimefromTicks(self):
+        date = datetime.fromtimestamp(self.ticks).strftime('%b %d %Y')
 
-def wasMarketClosedFrom(start, end, days):
-    market_open_days = []
-    market_closed_days = []
+        return date
+    
+    def getDayFromDate(self):
+        day = datetime.strptime(self.date, '%Y-%m-%d').strftime('%A')
 
-    for i in range(days):
-        current = start + i * 86400
-        current_date = getFancyDateTimefromTicks(current)
+        return day
 
-        market_closed = isMarketClosed(current)
+    def getMarketCloseDates(self):
+        with open('market-closed-dates.txt') as file:
+            lines = file.readlines()
 
-        if market_closed:
-            market_closed_days.append(current_date)
+        return lines
+    
+    def isMarketClosed(self):
+        date = self.getDateTimefromTicks().split(' ')[0]
+        
+        market_closed_dates = self.getMarketCloseDates()
+        day = self.getDayFromDate()
+
+        if date in market_closed_dates:
+            return True
+        elif day == 'Saturday' or day == 'Sunday':
+            return True
         else:
-            market_open_days.append(current_date)
+            return False
+        
+    def wasMarketClosedFrom(self):
+        market_open_days = []
+        market_closed_days = []
 
-    return market_open_days, market_closed_days
+        for i in range(self.date):
+            current = self.start + i * 86400
+            current_date = self.getFancyDateTimefromTicks()
 
-def telemetry(ticker, days):
-    ticks = int(time.time())
+            market_closed = self.isMarketClosed()
 
-    with open('telemetry.txt') as f:
-        lines = f.readlines()
+            if market_closed:
+                market_closed_days.append(current_date)
+            else:
+                market_open_days.append(current_date)
 
-        timestamp = lines[-1].rstrip().split(' ')[2]
-
-    if timestamp == str(ticks):
-        print('RATELIMITED')
-        time.sleep(1.25)
+        return market_open_days, market_closed_days
+    
+    def telemetry(self):
         ticks = int(time.time())
 
-    with open('telemetry.txt', 'w') as file:
-        lines.append(f'{ticker} {days} {ticks}\n')
-        file.write(''.join(lines))
+        with open('telemetry.txt') as f:
+            lines = f.readlines()
 
-        return True
+            timestamp = lines[-1].rstrip().split(' ')[2]
 
+        if timestamp == str(ticks):
+            print('RATELIMITED')
+            time.sleep(1.25)
+            ticks = int(time.time())
+
+        with open('telemetry.txt', 'w') as file:
+            lines.append(f'{self.ticks} {self.date} {ticks}\n')
+            file.write(''.join(lines))
+
+            return True
+        
 
 

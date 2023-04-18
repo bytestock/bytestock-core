65623c6417a354c0f561779ea1162793606a5cfd
5late <christianrfernandes5@gmail.com>
2023-04-17 23:22:19 -0400

    updates

--- data.py
+++ data.py
@@ -1,53 +1,100 @@
 """Libraries"""
-import yfinance as yf # importing the yfinance module to get the stock data
-import streamlit as st # importing the streamlit module to create the web app
-import time # importing the time module to get the current time
-from misc import Misc # importing the Misc class from the misc module
-import datetime # importing the datetime module to get the current date and time
+import yfinance as yf
+import streamlit as st
+import os
+import time
+import json
+from misc import Misc
+import datetime
 
-class Data:
-    """Data Class"""
-    def __init__(self,stocks, days) -> None: # initializing the class
-        self.ticks = int(time.time())
-        self.miscellaneous = Misc(self.ticks, "", "", "")
-        self.ticker = stocks
-        self.days = days
+def getOCHLData(days, ticker) ->list:
+    """Gets Data"""
+    now = datetime.datetime.now()
+    d = datetime.timedelta(days = days)
+    start = now - d
+    start = start.strftime('%Y-%m-%d')
 
-    def getOCHLData(self) ->list: #Historical Data for stock and crypto prices
-        """Gets Data"""
-        now = datetime.datetime.now()
-        d = datetime.timedelta(days = self.days)
-        start = now - d
-        start = start.strftime('%Y-%m-%d')
+    misc = Misc(now, d, start, now)
 
-        rate_limit_free = self.miscellaneous.telemetry()
-        if rate_limit_free:
-            stock = yf.Ticker(self.ticker)
-            data = yf.download(self.ticker, start=start, end=now.strftime('%Y-%m-%d'))
-            
-        open_days = data.index.tolist()
-        daily_close = list(data['Close'])
-        daily_adj_close = list(data['Adj Close'])
-        daily_open = list(data['Open'])
-        daily_high = list(data['High'])
-        daily_low = list(data['Low'])
+    rate_limit_free = misc.telemetry()
+    if rate_limit_free:
+        stock = yf.Ticker(ticker)
+        data = yf.download(ticker, start=start, end=now.strftime('%Y-%m-%d'))
 
-        return open_days, daily_open, daily_close, daily_adj_close, daily_high, daily_low
- 
-    @st.cache_data # caching the data
-    def getRealTimeOCHL(self) ->list: #Real Time Data for live stock and crypto prices
-        """Gets Real Time Data"""
-        rate_limit_free = self.miscellaneous.telemetry()
-        if rate_limit_free: 
-            stock = yf.Ticker(self.ticker)
-            data = stock.fast_info
-    
-        rt_previous_close = list(stock.history(period=f'2d')['Close'])[-2]
-        rt_open = data.open
-        rt_current = data.last_price
-        rt_high = data.day_high
-        rt_low = data.day_low
-        rt_change = rt_current - data.previous_close
-        rt_change_percent = round((rt_current - rt_previous_close)/((rt_current + rt_previous_close) / 2) * 100, 2)
+    #open_days, closed_days = misc.wasMarketClosedFrom(ticks - start, ticks, days-1)
+    open_days = data.index.tolist()
+    daily_close = list(data['Close'])
+    daily_adj_close = list(data['Adj Close'])
+    daily_open = list(data['Open'])
+    daily_high = list(data['High'])
+    daily_low = list(data['Low'])
 
-        return rt_previous_close, rt_open, rt_current, rt_high, rt_low, rt_change, rt_change_percent
+    return open_days, daily_open, daily_close, daily_adj_close, daily_high, daily_low
+
+@st.cache_data
+def getRealTimeOCHL(ticker, days:int) ->list:
+    """Gets Real Time Data"""
+    misc = Misc(ticker, days, "", "")
+    rate_limit_free = misc.telemetry()
+    if rate_limit_free:
+        stock = yf.Ticker(ticker)
+        data = stock.fast_info
+
+    rt_previous_close = list(stock.history(period=f'2d')['Close'])[0]
+    rt_open = data.open
+    rt_current = data.last_price
+    rt_high = data.day_high
+    rt_low = data.day_low
+    rt_change = rt_current - data.previous_close
+    rt_change_percent = round((rt_current - rt_previous_close)/((rt_current + rt_previous_close) / 2) * 100, 2)
+
+    return rt_previous_close, rt_open, rt_current, rt_high, rt_low, rt_change, rt_change_percent
+
+"""
+
+def getOCHLData(ticker, days: int) ->list:
+    miscellaneous = Misc(ticks, days, "", "")
+"""
+"""
+    now = datetime.datetime.now()
+    d = datetime.timedelta(days = days)
+    start = now - d
+    start = start.strftime('%Y-%m-%d')
+
+    rate_limit_free = miscellaneous.telemetry()
+    if rate_limit_free:
+        #data = finnhub_client.stock_candles(ticker, 'D', ticks - start, ticks)
+        stock = yf.Ticker(ticker)
+        data = yf.download(ticker, start=start, end=now.strftime('%Y-%m-%d'))
+
+    #open_days, closed_days = misc.wasMarketClosedFrom(ticks - start, ticks, days-1)
+    open_days = data.index.tolist()
+    daily_close = list(data['Close'])
+    daily_adj_close = list(data['Adj Close'])
+    daily_open = list(data['Open'])
+    daily_high = list(data['High'])
+    daily_low = list(data['Low'])
+
+    return open_days, daily_open, daily_close, daily_adj_close, daily_high, daily_low
+
+@st.cache_data
+def getRealTimeOCHL(ticker, days:int) ->list:
+    miscellaneous = Misc(ticker, days, "", "")
+    """
+"""
+    rate_limit_free = miscellaneous.telemetry()
+    if rate_limit_free:
+        stock = yf.Ticker(ticker)
+        data = stock.fast_info
+
+    rt_previous_close = list(stock.history(period=f'2d')['Close'])[-2]
+    rt_open = data.open
+    rt_current = data.last_price
+    rt_high = data.day_high
+    rt_low = data.day_low
+    rt_change = rt_current - data.previous_close
+    rt_change_percent = round((rt_current - rt_previous_close)/((rt_current + rt_previous_close) / 2) * 100, 2)
+
+    return rt_previous_close, rt_open, rt_current, rt_high, rt_low, rt_change, rt_change_percent
+
+"""
